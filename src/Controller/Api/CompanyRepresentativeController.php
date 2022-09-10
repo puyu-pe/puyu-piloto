31a5f56d307737e4def1a5548c19f017ff73103f
Emerson NV <nahuinlla101131@gmail.com>
2022-09-09 20:13:56 -0500

    YUN-32 #comment Install, config FosREST and add an example #time 1h #done

--- src/Controller/Api/CompanyRepresentativeController.php
+++ src/Controller/Api/CompanyRepresentativeController.php
@@ -1,38 +1,36 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Api;
 
 use App\Entity\CompanyRepresentative;
 use App\Repository\CompanyRepresentativeRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class CompanyRepresentativeController extends AbstractController
+class CompanyRepresentativeController extends AbstractFOSRestController
 {
-    #[Route('/', name: 'company_representative_list', methods: 'GET')]
-    public function list(
+    #[Rest\Get(path: '/api/company_representative', name: 'company_representative_list')]
+    #[Rest\View(serializerGroups: ['company_representative'])]
+    public function getAction(
         CompanyRepresentativeRepository $companyRepresentativeRepository,
-        NormalizerInterface             $normalizer,
-    ): Response
+    )
     {
-        $representatives = $companyRepresentativeRepository->findAll();
-        $data = $normalizer->normalize($representatives, null, ['groups' => ['default']]);
-        return new JsonResponse($data);
+        return $companyRepresentativeRepository->findAll();
     }
 
-    #[Route('/', name: 'company_representative_save', methods: 'POST')]
-    public function save(
+    #[Rest\Post(path: '/api/company_representative', name: 'company_representative_save')]
+    public function postAction(
         Request                $request,
         EntityManagerInterface $entityManager,
         ValidatorInterface     $validator,
         NormalizerInterface    $normalizer,
-    ): Response
+    ): View
     {
         $data = json_decode($request->getContent());
         $companyRepresentative = new CompanyRepresentative();
@@ -44,31 +42,32 @@ class CompanyRepresentativeController extends AbstractController
         $errors = $validator->validate($companyRepresentative);
 
         if (count($errors) > 0) {
-
             $data = $normalizer->normalize($errors, null, ['groups' => ['default']]);
-            return new JsonResponse($data);
+            return View::create($data, Response::HTTP_BAD_REQUEST);
         }
+
         $entityManager->persist($companyRepresentative);
         $entityManager->flush();
 
-        return new Response('Added company representative: ' . $companyRepresentative->getId());
+        return View::create('Added company representative: ' . $companyRepresentative->getId(), Response::HTTP_CREATED);
     }
 
-    #[Route('/{id}', name: 'company_representative_update', requirements: ['id' => '\d+'], methods: 'PUT')]
-    public function update(
+
+    #[Rest\Put(path: '/api/company_representative/{id}', name: 'company_representative_update', requirements: ['id' => '\d+'])]
+    public function editAcion(
         int                             $id,
         Request                         $request,
         CompanyRepresentativeRepository $companyRepresentativeRepository,
         NormalizerInterface             $normalizer,
         ValidatorInterface              $validator,
         EntityManagerInterface          $entityManager
-    ): Response
+    ): View
     {
         $data = json_decode($request->getContent(), false, 512, JSON_THROW_ON_ERROR);
         $companyRepresentative = $companyRepresentativeRepository->find($id);
 
         if (!$companyRepresentative) {
-            return new Response('Not found company representative, id: ' . $id);
+            return View::create('Not found company representative, id: ' . $id, Response::HTTP_BAD_REQUEST);
         }
 
         $companyRepresentative->setName($data->name)
@@ -80,31 +79,31 @@ class CompanyRepresentativeController extends AbstractController
 
         if (count($errors) > 0) {
             $data = $normalizer->normalize($errors, null, ['groups' => ['default']]);
-            return new JsonResponse($data);
+            return View::create($data, Response::HTTP_BAD_REQUEST);
         }
 
         $entityManager->persist($companyRepresentative);
         $entityManager->flush();
 
-        return new Response('Update company representative: ' . $companyRepresentative->getId());
+        return View::create('Update company representative: ' . $companyRepresentative->getId(), Response::HTTP_ACCEPTED);
     }
 
-    #[Route('/{id}', name: 'company_representative_delete', requirements: ['id' => '\d+'], methods: 'DELETE')]
-    public function delete(
+    #[Rest\Delete(path: '/api/company_representative/{id}', name: 'company_representative_delete', requirements: ['id' => '\d+'])]
+    public function deleteAction(
         int                             $id,
         CompanyRepresentativeRepository $companyRepresentativeRepository,
         EntityManagerInterface          $entityManager
-    ): Response
+    ): View
     {
         $companyRepresentative = $companyRepresentativeRepository->find($id);
 
         if (!$companyRepresentative) {
-            return new Response('Not found company representative, id: ' . $id);
+            return View::create('Not found company representative, id: ' . $id, Response::HTTP_BAD_REQUEST);
         }
 
         $entityManager->remove($companyRepresentative);
         $entityManager->flush();
 
-        return new Response('Delete company representative id: ' . $id);
+        return View::create('Delete company representative id: ' . $id, Response::HTTP_NO_CONTENT);
     }
 }
