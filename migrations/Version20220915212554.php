5209d38fb4c8121ecb9105d82fccc889e531b33f
Roel W <22hroel@gmail.com>
2022-09-15 16:53:08 -0500

    YUN-13 #comment add entity and migration for table products #time 4h #done

--- migrations/Version20220915212554.php
+++ migrations/Version20220915212554.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220910225048 extends AbstractMigration
+final class Version20220915212554 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,11 +21,13 @@ final class Version20220910225048 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE customer_contact (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, last_name VARCHAR(300) NOT NULL, phone VARCHAR(20) DEFAULT NULL, job_title VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, code VARCHAR(20) NOT NULL, name VARCHAR(250) NOT NULL, description VARCHAR(400) DEFAULT NULL, url VARCHAR(250) NOT NULL, image VARCHAR(400) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE customer_contact');
+        $this->addSql('DROP TABLE product');
     }
 }
