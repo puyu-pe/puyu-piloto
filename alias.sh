f9c4cb2d8abaf425dc705f3937a31197975a28f3
Velnae-28 <nahuinlla101131@gmail.com>
2022-09-30 14:10:04 -0500

    YUN-91 #comment agregar comando make ci

--- alias.sh
+++ alias.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-DOCKER_COMPOSE_PATH=".docker"
+DOCKER_COMPOSE_PATH='.docker'
 source ${DOCKER_COMPOSE_PATH}/.env
-DOCKER_PHP="${APP_NAME}_php"
+DOCKER_PHP='${APP_NAME}_php'
 
 # PHP
-alias php="U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} php";
+alias php='U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} php';
 
 # Composer
-alias composer="U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} composer";
+alias composer='U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} composer';
 
 # Symfony
-alias sf="U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} php /var/www/html/bin/console";
+alias sf='U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} php /var/www/html/bin/console';
 
 # PHPUnit
-alias test="U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} php /var/www/html/bin/phpunit";
+alias test='U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} php /var/www/html/bin/phpunit';
 
 # ECS - Easy Coding Standard (Sniffer & Fixers)
-alias ecs="U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} php vendor/bin/ecs";
+alias ecs='U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} php vendor/bin/ecs';
 
 # PHPStan - Static Analyses Tool
-alias stan="U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} php vendor/bin/phpstan";
+alias stan='U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} php vendor/bin/phpstan';
