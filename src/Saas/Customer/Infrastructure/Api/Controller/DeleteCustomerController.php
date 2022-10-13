9ac3c49212d11105c5f6f0360b3f3a280ac6821f
Cielo <aiquipacielo.25@gmail.com>
2022-10-13 10:19:14 -0500

    YUN-98 #comment Add API doc customer YUN-98 #time 3h YUN-98 #done

--- src/Saas/Customer/Infrastructure/Api/Controller/DeleteCustomerController.php
+++ src/Saas/Customer/Infrastructure/Api/Controller/DeleteCustomerController.php
@@ -4,15 +4,28 @@ namespace App\Saas\Customer\Infrastructure\Api\Controller;
 
 use App\Saas\Customer\Application\Delete\DeleteCustomer;
 use App\Saas\Customer\Domain\Exception\CustomerNotFound;
+use App\Saas\Shared\Infrastructure\Api\Controller\ApiController;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Requirement\Requirement;
 
-class DeleteCustomerController extends AbstractFOSRestController
+class DeleteCustomerController extends ApiController
 {
+    /**
+     * Delete a customer
+     *
+     * Delete a customer
+     */
     #[Rest\Delete(path: '/{id}', name: 'customer_delete', requirements: ['id' => Requirement::UUID_V4])]
+    #[OA\Response(
+        response: 200,
+        description: 'Successful response'
+    )]
+    #[OA\Tag(name: 'Customer')]
+
     public function __invoke(
         string $id,
         DeleteCustomer $useCase,
--- src/Saas/Customer/Infrastructure/Api/Controller/GetAllCustomerController.php
+++ src/Saas/Customer/Infrastructure/Api/Controller/GetAllCustomerController.php
@@ -3,14 +3,44 @@
 namespace App\Saas\Customer\Infrastructure\Api\Controller;
 
 use App\Saas\Customer\Application\GetAll\GetAllCustomer;
+use App\Saas\Customer\Domain\Entity\Customer;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Response;
 
 class GetAllCustomerController extends AbstractFOSRestController
 {
+    /**
+     * Get all customer
+     *
+     * Get all customer
+     */
     #[Rest\Get(path: '', name: 'customer_get_all')]
+    #[OA\Response(
+        response: 200,
+        description: 'Successful response',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'status', example: 'success'),
+                new OA\Property(
+                    property: 'data',
+                    properties: [
+                        new OA\Property(
+                            property: 'Customer',
+                            title: 'customer',
+                            type: 'array',
+                            items: new OA\Items(ref: new Model(type: Customer::class))
+                        )
+                    ],
+                    type: 'object',
+                )
+            ]
+        )
+    )]
+    #[OA\Tag(name: 'Customer')]
     public function __invoke(
         GetAllCustomer $useCase,
     ): Response {
