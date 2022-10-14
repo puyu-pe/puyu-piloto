dce6cee0663cd109a87716687207128ffe980ded
Roel W <22hroel@gmail.com>
2022-10-14 11:11:20 -0500

    YUN-99 #comment documentation product api #time 2h #done

--- src/Saas/Product/Infrastructure/Api/Controller/FindProductController.php
+++ src/Saas/Product/Infrastructure/Api/Controller/FindProductController.php
@@ -3,17 +3,36 @@
 namespace App\Saas\Product\Infrastructure\Api\Controller;
 
 use App\Saas\Product\Application\Find\FindProduct;
+use App\Saas\Product\Domain\Entity\Product;
 use App\Saas\Product\Domain\Exception\ProductNotFound;
-use FOS\RestBundle\Controller\AbstractFOSRestController;
+use App\Saas\Shared\Infrastructure\Api\Controller\ApiController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Requirement\Requirement;
 use Symfony\Component\Uid\Uuid;
 
-class FindProductController extends AbstractFOSRestController
+class FindProductController extends ApiController
 {
+    /**
+     * Find a product
+     *
+     * Find a product
+     */
     #[Rest\Get(path: '/{id}', name: 'product_find', requirements: ['id' => Requirement::UUID_V4])]
+    #[OA\Response(
+        response: 200,
+        description: 'Successful response',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'status', example: 'success'),
+                new OA\Property(property: 'data', ref: new Model(type: Product::class))
+            ]
+        )
+    )]
+    #[OA\Tag(name: 'Product')]
     public function __invoke(
         Uuid $id,
         FindProduct $useCase,
--- src/Saas/Product/Infrastructure/Api/Controller/GetAllProductController.php
+++ src/Saas/Product/Infrastructure/Api/Controller/GetAllProductController.php
@@ -3,14 +3,44 @@
 namespace App\Saas\Product\Infrastructure\Api\Controller;
 
 use App\Saas\Product\Application\GetAll\GetAllProducts;
-use FOS\RestBundle\Controller\AbstractFOSRestController;
+use App\Saas\Product\Domain\Entity\Product;
+use App\Saas\Shared\Infrastructure\Api\Controller\ApiController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Response;
 
-class GetAllProductController extends AbstractFOSRestController
+class GetAllProductController extends ApiController
 {
+    /**
+     * Get all products
+     *
+     * Get all products
+     */
     #[Rest\Get(path: '', name: 'product_get_all')]
+    #[OA\Response(
+        response: 200,
+        description: 'Successful response',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'status', example: 'success'),
+                new OA\Property(
+                    property: 'data',
+                    properties: [
+                        new OA\Property(
+                            property: 'Products',
+                            title: 'product',
+                            type: 'array',
+                            items: new OA\Items(ref: new Model(type: Product::class))
+                        )
+                    ],
+                    type: 'object',
+                )
+            ]
+        )
+    )]
+    #[OA\Tag(name: 'Product')]
     public function __invoke(
         GetAllProducts $useCase,
     ): Response {
