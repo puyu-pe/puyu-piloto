3a657044febbb58d81fd46ca34a69c9d421b366e
Velnae <nahuinlla101131@gmail.com>
2022-10-30 18:08:42 -0500

    YUN-144 #comment Actualizo la ultima migracion de directory a como deberia generar la migracion el comando  `do:mi:di` adicionalmente se puso a prueba con el comando `do:mi:va` que valida la integridad de todo el ecosistema Doctrine YUN-144 #time 10m

---
+++ migrations/Version20221030210209.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221030210209 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE directory (id BINARY(16) COMMENT \'(DC2Type:uuid)\' NOT NULL, customer_id BINARY(16) COMMENT \'(DC2Type:uuid)\' NOT NULL, contact_id BINARY(16) COMMENT \'(DC2Type:uuid)\' NOT NULL, INDEX IDX_467844DA9395C3F3 (customer_id), INDEX IDX_467844DAE7A1254A (contact_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE directory ADD CONSTRAINT FK_467844DA9395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (id)');
+        $this->addSql('ALTER TABLE directory ADD CONSTRAINT FK_467844DAE7A1254A FOREIGN KEY (contact_id) REFERENCES contact (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE directory DROP FOREIGN KEY FK_467844DA9395C3F3');
+        $this->addSql('ALTER TABLE directory DROP FOREIGN KEY FK_467844DAE7A1254A');
+        $this->addSql('DROP TABLE directory');
+    }
+}
